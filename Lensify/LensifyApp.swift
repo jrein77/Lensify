5c95b611fa7761404d00ffe759853467cbda828f
Jake Reinhart <jaker2208@gmail.com>
2024-07-25 12:21:15 -0400

    Initial Commit

---
+++ Lensify/LensifyApp.swift
@@ -0,0 +1,32 @@
+//
+//  LensifyApp.swift
+//  Lensify
+//
+//  Created by Jake Reinhart on 7/25/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LensifyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
